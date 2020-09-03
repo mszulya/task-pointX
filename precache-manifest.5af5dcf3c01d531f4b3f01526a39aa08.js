7cb245aeee0dcd0d9a9e5286d821869c10d2422c
mzulya <sul.zulya@gmail.com>
2020-09-03 15:01:05 +0700

    Updates

---
+++ precache-manifest.5af5dcf3c01d531f4b3f01526a39aa08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b863f9ec524d15587e757a6f06f90b",
+    "url": "/task-pointX/index.html"
+  },
+  {
+    "revision": "d117648104540818004f",
+    "url": "/task-pointX/static/css/main.c8eb8eb4.chunk.css"
+  },
+  {
+    "revision": "773d73f54448e2aac6f8",
+    "url": "/task-pointX/static/js/2.716d3666.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/task-pointX/static/js/2.716d3666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d117648104540818004f",
+    "url": "/task-pointX/static/js/main.874255e3.chunk.js"
+  },
+  {
+    "revision": "1d16b86f9f4168168a17",
+    "url": "/task-pointX/static/js/runtime-main.d8f15915.js"
+  },
+  {
+    "revision": "bab5359b46ee75cb8921ee38cde318f0",
+    "url": "/task-pointX/static/media/logo-transparent.bab5359b.svg"
+  }
+]);
